c5716f52245b66778a8d511e50e11cc37bf0a4cf
Andrew Siwko <asiwko@siwko.org>
2026-01-17 15:48:21 -0500

    remove outputs for now

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,10 +1,10 @@
-output "ARM_SUBSCRIPTION_ID" {
-  value = var.ARM_SUBSCRIPTION_ID
-}
-output "ARM_TENANT_ID" {
-  value = var.ARM_TENANT_ID
-}
+# output "ARM_SUBSCRIPTION_ID" {
+#   value = var.ARM_SUBSCRIPTION_ID
+# }
+# output "ARM_TENANT_ID" {
+#   value = var.ARM_TENANT_ID
+# }
 
-output "ARM_CLIENT_ID" {
-  value = var.ARM_CLIENT_ID
-}
+# output "ARM_CLIENT_ID" {
+#   value = var.ARM_CLIENT_ID
+# }
