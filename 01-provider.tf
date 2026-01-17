3732914b8ff515bf2fbab434f303e8cf61a00fa8
Andrew Siwko <asiwko@siwko.org>
2026-01-17 15:00:15 -0500

    try putting outputs higher up

--- 01-provider.tf
+++ 01-provider.tf
@@ -45,6 +45,16 @@ variable "ARM_CLIENT_SECRET" {
   type    = string
 }
 
+output "ARM_SUBSCRIPTION_ID" {
+  value = var.ARM_SUBSCRIPTION_ID
+}
+output "ARM_TENANT_ID" {
+  value = var.ARM_TENANT_ID
+}
+
+output "ARM_CLIENT_ID" {
+  value = var.ARM_CLIENT_ID
+}
 
 # resource "azurerm_resource_group" "state-demo-secure" {
 #   name     = "state-demo"
