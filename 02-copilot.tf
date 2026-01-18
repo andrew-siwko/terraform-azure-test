57df89295dd3208ada04bd754c400487a30094db
Andrew Siwko <asiwko@siwko.org>
2026-01-17 20:04:58 -0500

    azure wants RSA keys

--- 02-copilot.tf
+++ 02-copilot.tf
@@ -197,7 +197,7 @@ resource "azurerm_linux_virtual_machine" "vm_03" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("/container_shared/ansible/id_ed25519.desktop.pub")
+    public_key = file("/container_shared/ansible/id_rsa.desktop.pub")
   }
 
   os_disk {
