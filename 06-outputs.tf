bb6695c4d29ccbf183729383d713ce6bf7436b79
Andrew Siwko <asiwko@siwko.org>
2026-01-17 20:53:56 -0500

    output ip addresses

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -8,3 +8,9 @@
 # output "ARM_CLIENT_ID" {
 #   value = var.ARM_CLIENT_ID
 # }
+output "asiwko-vm-public-ip_03" {
+  value = azurerm_linux_virtual_machine.vm_03.public_ip_address
+}
+output "asiwko-vm-public-ip_04" {
+  value = azurerm_linux_virtual_machine.vm_04.public_ip_address
+}
