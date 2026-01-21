12602bc61468121a24773b4c40a33032d6241afc
Andrew Siwko <asiwko@siwko.org>
2026-01-21 13:16:49 -0500

    split up content / renamed files to parallel AWS layout.

---
+++ 03-security-group.tf
@@ -0,0 +1,96 @@
+# -------------------------
+# Network Security Group (Security Group)
+# -------------------------
+resource "azurerm_network_security_group" "public_access" {
+  name                = "allow_ssh_http_ping"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "ICMP"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "OutboundAll"
+    priority                   = 140
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -------------------------
+# Route Table (Public Route)
+# -------------------------
+resource "azurerm_route_table" "public_rt" {
+  name                = "public-route-table"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  route {
+    name           = "default-route"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internet"
+  }
+}
+
+# Associate route table with subnet
+resource "azurerm_subnet_route_table_association" "public_assoc" {
+  subnet_id      = azurerm_subnet.public_subnet.id
+  route_table_id = azurerm_route_table.public_rt.id
+}
+
+# Associate NSG with subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.public_subnet.id
+  network_security_group_id = azurerm_network_security_group.public_access.id
+}
+
---
+++ 04-machine.tf
@@ -0,0 +1,59 @@
+# -------------------------
+# Network Interfaces (NICs)
+# -------------------------
+resource "azurerm_network_interface" "nic_01" {
+  name                = "nic-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip_01.id
+  }
+}
+
+
+# Attach NSG to NICs 
+resource "azurerm_network_interface_security_group_association" "nic_01_assoc" {
+  network_interface_id      = azurerm_network_interface.nic_01.id
+  network_security_group_id = azurerm_network_security_group.public_access.id
+}
+
+
+# -------------------------
+# Virtual Machines 
+# -------------------------
+resource "azurerm_linux_virtual_machine" "vm_01" {
+  name                = "asiwko-vm-01"
+  computer_name       = "asiwko-vm-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_L2aos_v4"
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic_01.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/container_shared/ansible/ansible_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "asiwko-vm-01-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "97-gen2"
+    version   = "latest"
+  }
+}
+
