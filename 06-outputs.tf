f5f5798f61ee5c5acbdeb36c7cb31eb4ed1869a0
Andrew Siwko <asiwko@siwko.org>
2026-02-04 16:30:37 -0500

    remove commented out outputs

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,16 +1,3 @@
-# output "ARM_SUBSCRIPTION_ID" {
-#   value = var.ARM_SUBSCRIPTION_ID
-# }
-# output "ARM_TENANT_ID" {
-#   value = var.ARM_TENANT_ID
-# }
-
-# output "ARM_CLIENT_ID" {
-#   value = var.ARM_CLIENT_ID
-# }
 output "asiwko-vm-public-ip_01" {
   value = azurerm_linux_virtual_machine.vm_01.public_ip_address
 }
-# output "asiwko-vm-public-ip_04" {
-#   value = azurerm_linux_virtual_machine.vm_04.public_ip_address
-# }
